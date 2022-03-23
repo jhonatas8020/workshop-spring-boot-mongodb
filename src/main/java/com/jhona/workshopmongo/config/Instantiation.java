6dbbee05172c3c7923d794e1da7016c7277824b6
Jhonatas Coutinho <jhonatas.cjw@outlook.com>
2022-03-23 11:34:13 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/jhona/workshopmongo/config/Instantiation.java
+++ src/main/java/com/jhona/workshopmongo/config/Instantiation.java
@@ -1,12 +1,15 @@
 package com.jhona.workshopmongo.config;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.jhona.workshopmongo.domain.Post;
 import com.jhona.workshopmongo.domain.User;
+import com.jhona.workshopmongo.repositories.PostRepository;
 import com.jhona.workshopmongo.repositories.UserRepository;
 
 @Configuration
@@ -14,17 +17,25 @@ public class Instantiation implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post post1 = new Post(null, LocalDateTime.parse("2018-03-21T10:35:10"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, LocalDateTime.parse("2018-03-23T09:10:47"), "Bom dia", "Acordei feliz hoje!", maria);
+		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
 
